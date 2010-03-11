a93672dc0095580f718e42c5b49df61acb228c07
etienne <etienne@localhost>
2010-03-11 11:25:50 +0100

    updated scripts

--- src/fr/imag/adele/cadse/testplatform/tests_basictests/All.groovy
+++ src/fr/imag/adele/cadse/testplatform/tests_basictests/All.groovy
@@ -1,26 +1,24 @@
 package fr.imag.adele.cadse.testplatform.tests_basictests
 
 import fr.imag.adele.cadse.platform.*
-import java.util.ArrayList
 
 
 public class All extends CadseTest {
-
+	
 	public void addBundleToCompile() {
 	}
 	
 	public boolean runTest() {
 		return false;
 	}
-
+	
 	public static void main(String[] args) {
 		main(	new BasicProperties(), 
-				new CannotBeUndefined(),
-				new CheckPages(),
-				new DefaultInstanceName(),
-				new HasContent(),
-				new IsAbstract(),
-				new Link(),
-				new Root())
+		new CheckPages(),
+		new DefaultInstanceName(),
+		new HasContent(),
+		new IsAbstract(),
+		new Link(),
+		new Root())
 	}
 }
