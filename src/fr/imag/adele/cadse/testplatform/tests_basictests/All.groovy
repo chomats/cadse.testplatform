d4011e13700c402aca3102d7af5ff63458ed56c9
etienne <etienne@localhost>
2010-03-30 10:18:34 +0200

    comment tests to speed up the continuum

--- src/fr/imag/adele/cadse/testplatform/tests_basictests/All.groovy
+++ src/fr/imag/adele/cadse/testplatform/tests_basictests/All.groovy
@@ -7,10 +7,10 @@ public class All extends CadseTest {
 	
 	public static void main(String[] args) {
 		main(
-		new BasicProperties_double(), 
-		new BasicProperties_integer(),
-		new BasicProperties_long(),
-		new BasicProperties_string(),
+		//new BasicProperties_double(), 
+		//new BasicProperties_integer(),
+		//new BasicProperties_long(),
+		//new BasicProperties_string(),
 		new CheckPages(),
 		new DefaultInstanceName(),
 		new HasContent(),
