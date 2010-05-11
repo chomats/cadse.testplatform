bac4dbd68341930041acf198a7003b03160ec3cb
etienne <etienne@localhost>
2010-05-11 11:11:20 +0200

    New set of test for group link

--- src/fr/imag/adele/cadse/testplatform/tests_basictests/GroupTest1_Integer.groovy
+++ src/fr/imag/adele/cadse/testplatform/tests_basictests/GroupTest1_Integer.groovy
@@ -4,7 +4,7 @@ import fr.imag.adele.cadse.platform.gr.CadsegTestCase;
 import fr.imag.adele.cadse.platform.*
 
 
-public class Group_basic extends CadsegTestCase {
+public class GroupTest1_Integer extends CadsegTestCase {
 	
 	public void init() {
 		/* Libraries */
@@ -16,19 +16,19 @@ public class Group_basic extends CadsegTestCase {
 		run.addBundle(run.wsTest, "fr.imag.adele.cadse.test.basictests", "src")
 		
 		/* TEST */
-		CadseTestPart tp = addTestPart(null, 'Group_basic_CADSEg','fr.imag.adele.cadse.test.basictests','fr.imag.adele.cadse.test.basictests.group.basic.Group_basic_ts_CADSEg')
-		tp.deleteBundle("Model.Workspace.CADSE_group_basic")
+		CadseTestPart tp = addTestPart(null, 'Group_basic_CADSEg','fr.imag.adele.cadse.test.basictests','fr.imag.adele.cadse.test.basictests.group.test1.Test1_Integer_ts_CADSEg')
+		tp.deleteBundle("Model.Workspace.CADSE_group_test1_Integer")
 		
 		
-		tp = addTestPart(null, 'Group_basic_Execution','fr.imag.adele.cadse.test.basictests','fr.imag.adele.cadse.test.basictests.group.basic.Group_basic_ts_execution') {
+		tp = addTestPart(null, 'Group_basic_Execution','fr.imag.adele.cadse.test.basictests','fr.imag.adele.cadse.test.basictests.group.test1.Test1_Integer_ts_execution') {
 			delegate.wsDir = "${run.testPlatformPath}/test-ws-2"
 			run.ant.mkdir(dir: delegate.wsDir)
 		}
-		tp.addBundle("${run.testPlatformPath}/test-ws", "Model.Workspace.CADSE_group_basic", "src-gen")
+		tp.addBundle("${run.testPlatformPath}/test-ws", "Model.Workspace.CADSE_group_test1_Integer", "src-gen")
 		
 	}
 	
 	public static void main(String[] args) {
-		main(new Group_basic())
+		main(new GroupTest1_Integer())
 	}
 }
