0623a5ef561205cda607db439c93c93fbfd4487e
etienne <etienne@localhost>
2010-05-19 14:31:25 +0200

    Group : New tests (starting implementation)

---
+++ src/fr/imag/adele/cadse/testplatform/tests_basictests/GroupTest1_Enum.groovy
@@ -0,0 +1,34 @@
+package fr.imag.adele.cadse.testplatform.tests_basictests
+import fr.imag.adele.cadse.platform.gr.CadsegTestCase;
+
+import fr.imag.adele.cadse.platform.*
+
+
+public class GroupTest1_Enum extends CadsegTestCase {
+	
+	public void init() {
+		/* Libraries */
+		run.addBundle(run.wsTest, "fr.imag.adele.graphictests", "src/main/java")
+		run.addBundle(run.wsTest, "fr.imag.adele.graphictests.cadse", "src/main/java")
+		run.addBundle(run.wsTest, "org.eclipse.swtbot.swt.finder.keyboard.fr", "src")
+		
+		/* Bundle to be created */
+		run.addBundle(run.wsTest, "fr.imag.adele.cadse.test.basictests", "src")
+		
+		/* TEST */
+		CadseTestPart tp = addTestPart(null, 'Group_basic_CADSEg','fr.imag.adele.cadse.test.basictests','fr.imag.adele.cadse.test.basictests.group.test1.Test1_Enum_ts_CADSEg')
+		tp.deleteBundle("Model.Workspace.CADSEgroup_test1Enum")
+		
+		
+		tp = addTestPart(null, 'Group_basic_Execution','fr.imag.adele.cadse.test.basictests','fr.imag.adele.cadse.test.basictests.group.test1.Test1_Enum_ts_execution') {
+			delegate.wsDir = "${run.testPlatformPath}/test-ws-2"
+			run.ant.mkdir(dir: delegate.wsDir)
+		}
+		tp.addBundle("${run.testPlatformPath}/test-ws", "Model.Workspace.CADSEgroup_test1Enum", "src-gen")
+		
+	}
+	
+	public static void main(String[] args) {
+		main(new GroupTest1_Enum())
+	}
+}
