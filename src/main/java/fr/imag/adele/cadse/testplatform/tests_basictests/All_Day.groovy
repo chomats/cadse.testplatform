467753bdfc5c2c6809d859f1e1f66aeeed558733
Etienne Gandrille <etienne.gandrille@imag.fr>
2010-07-06 11:12:27 +0200

    Tests scripts update

--- src/main/java/fr/imag/adele/cadse/testplatform/tests_basictests/All_Day.groovy
+++ src/main/java/fr/imag/adele/cadse/testplatform/tests_basictests/All_Day.groovy
@@ -5,25 +5,60 @@ import fr.imag.adele.cadse.platform.*
 
 public class All_Day extends CadseTest {
 	
+	// IMPORTANT!!! READ ME FIRST!!!
+	//
+	// Some tests are commented in this file. They are only launched during night, and not during day... 
+	
 	public static void main(String[] args) {
 		main(
-		//new BasicProperties_Boolean(), // Night test only
-		//new BasicProperties_Double(),  // Night test only
-		//new BasicProperties_Enum(),    // Night test only
-		//new BasicProperties_Integer(), // Night test only
-		//new BasicProperties_Long(),    // Night test only
-		//new BasicProperties_String(),  // Night test only
-		//new GroupTest1_Boolean(),      // Night test only
-		//new GroupTest1_Double(),       // Night test only
-		//new GroupTest1_Enum(),         // Night test only
-		//new GroupTest1_Integer(),      // Night test only
-		//new GroupTest1_Long(),         // Night test only
-		//new GroupTest1_String(),       // Night test only
+		// ================ //
+		//    First tests   //
+		// ================ //
+		
 		new CheckPages(),
 		new DefaultInstanceName(),
 		new HasContent(),
 		new IsAbstract(),
 		new Link(),
-		new Root())
+		new Root(),
+		
+		// ================ //
+		// Basic Properties //
+		// ================ //
+		
+		//new BasicProperties_Boolean(),
+		//new BasicProperties_Double(),
+		//new BasicProperties_Enum(),
+		//new BasicProperties_Integer(),
+		//new BasicProperties_Long(),
+		//new BasicProperties_String(),
+		
+		// ================ //
+		//    Group Test1   //
+		// ================ //
+		
+		//new GroupTest1_Boolean(),
+		//new GroupTest1_Double(),
+		//new GroupTest1_Enum(),
+		//new GroupTest1_Integer(),
+		//new GroupTest1_Long(),
+		//new GroupTest1_String(),
+		
+		// ================ //
+		//    Group Test3   //
+		// ================ //
+		
+		//new GroupTest3_Boolean(),
+		//new GroupTest3_Double(),
+		//new GroupTest3_Enum(),
+		//new GroupTest3_Integer(),
+		//new GroupTest3_Long(),
+		//new GroupTest3_String(),
+		
+		// ================ //
+		//       Misc       //
+		// ================ //
+		
+		new Bug1())
 	}
 }
--- src/main/java/fr/imag/adele/cadse/testplatform/tests_basictests/All_Night.groovy
+++ src/main/java/fr/imag/adele/cadse/testplatform/tests_basictests/All_Night.groovy
@@ -7,23 +7,55 @@ public class All_Night extends CadseTest {
 	
 	public static void main(String[] args) {
 		main(
+		
+		// ================ //
+		//    First tests   //
+		// ================ //
+		
+		new CheckPages(),
+		new DefaultInstanceName(),
+		new HasContent(),
+		new IsAbstract(),
+		new Link(),
+		new Root(),
+		
+		// ================ //
+		// Basic Properties //
+		// ================ //
+		
 		new BasicProperties_Boolean(),
 		new BasicProperties_Double(),
 		new BasicProperties_Enum(),
 		new BasicProperties_Integer(),
 		new BasicProperties_Long(),
 		new BasicProperties_String(),
+		
+		// ================ //
+		//    Group Test1   //
+		// ================ //
+		
 		new GroupTest1_Boolean(),
 		new GroupTest1_Double(),
 		new GroupTest1_Enum(),
 		new GroupTest1_Integer(),
 		new GroupTest1_Long(),
 		new GroupTest1_String(),
-		new CheckPages(),
-		new DefaultInstanceName(),
-		new HasContent(),
-		new IsAbstract(),
-		new Link(),
-		new Root())
+		
+		// ================ //
+		//    Group Test3   //
+		// ================ //
+		
+		//new GroupTest3_Boolean(),
+		//new GroupTest3_Double(),
+		//new GroupTest3_Enum(),
+		//new GroupTest3_Integer(),
+		//new GroupTest3_Long(),
+		//new GroupTest3_String(),
+		
+		// ================ //
+		//       Misc       //
+		// ================ //
+		
+		new Bug1())
 	}
 }
